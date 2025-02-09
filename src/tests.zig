07b3fa8dd5e4de26e6f0026ac60b851c4a5ffd7c
amandeepsp <amandeepspdhr@gmail.com>
2025-02-09 09:56:12 +0530

    feat: add better record packing into the block

--- src/tests.zig
+++ src/tests.zig
@@ -1,3 +1,4 @@
 comptime {
     _ = @import("skiplist.zig");
+    _ = @import("wal.zig");
 }
--- src/wal.zig
+++ src/wal.zig
@@ -1,16 +1,19 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const fs = std.fs;
+const log = std.log;
+const testing = std.testing;
+const hash = std.hash;
 
 // WAL format adapted form https://github.com/facebook/rocksdb/wiki/Write-Ahead-Log-File-Format
 
 pub const WalOptions = struct {
     file_dir: fs.Dir = fs.cwd(),
     file_name: []const u8 = "wal.log",
-    max_block_size: usize = 32 * 2 * @sizeOf(u8),
+    block_size: usize = 32 * 1024 * @sizeOf(u8), // 32KB
 };
 
-// Since max_block_size is fixed, a record could get broken into multiple blocks,
+// Since block_size is fixed, a record could get broken into multiple blocks,
 // to reconstruct, we need info where does the record start and end.
 const RecordType = enum(u8) {
     FULL = 1,
@@ -20,13 +23,36 @@ const RecordType = enum(u8) {
 };
 
 const Record = struct {
+    // +---------+-------------+------------+--- ... ---+
+    // |CRC (u32) | Size (u16) | Type (u8) | Payload   |
+    // +---------+-------------+------------+--- ... ---+
+    // CRC = 32bit hash computed over the payload using CRC
+    // Size = Length of the payload data
+    // Type = Type of record, FULL, FIRST, MIDDLE, or LAST
+    //       The type is used to group a bunch of records together to represent
+    //       blocks that are larger than kBlockSize
+    // Payload = Byte stream as long as specified by the payload size
     checksum: u32,
-    length: usize,
-    type: RecordType,
+    length: u16,
+    record_type: RecordType,
     data: []const u8,
 
     pub fn size(self: *const Record) usize {
-        return @sizeOf(u32) + @sizeOf(usize) + @sizeOf(RecordType) + self.length;
+        return @sizeOf(u32) + @sizeOf(u16) + @sizeOf(u8) + self.length;
+    }
+
+    pub fn header_size() usize {
+        return @sizeOf(u32) + @sizeOf(u16) + @sizeOf(u8);
+    }
+
+    pub fn init(payload: []const u8, record_type: RecordType) Record {
+        const checksum = hash.Crc32.hash(payload);
+        return Record{
+            .checksum = checksum,
+            .length = @intCast(payload.len),
+            .record_type = record_type,
+            .data = payload,
+        };
     }
 
     pub fn encode(self: *const Record, buffer: []u8) !void {
@@ -34,24 +60,60 @@ const Record = struct {
         var buffer_writer = buffer_stream.writer();
 
         try buffer_writer.writeInt(u32, self.checksum, .little);
-        try buffer_writer.writeInt(usize, self.length, .little);
-        try buffer_writer.writeInt(u8, @intFromEnum(self.type), .little);
+        try buffer_writer.writeInt(u16, @truncate(self.length), .little);
+        try buffer_writer.writeInt(u8, @intFromEnum(self.record_type), .little);
         try buffer_writer.writeAll(self.data);
+    }
+
+    pub fn decode(buffer: []const u8) !Record {
+        const header_len = Record.header_size();
+        var buffer_stream = std.io.fixedBufferStream(buffer);
+        var buffer_reader = buffer_stream.reader();
+
+        const checksum = try buffer_reader.readInt(u32, .little);
+        const length = try buffer_reader.readInt(u16, .little);
+        if (length == 0) {
+            return error.InvalidRecord;
+        }
+        const record_type_int: u8 = try buffer_reader.readInt(u8, .little);
+        const record_type: RecordType = std.meta.intToEnum(RecordType, record_type_int) catch {
+            return error.InvalidRecord;
+        };
+        const data = buffer[header_len..(header_len + length)];
+
+        return Record{
+            .checksum = checksum,
+            .length = length,
+            .record_type = record_type,
+            .data = data,
+        };
     }
 };
 
 pub const WriteAheadLog = struct {
+    //       +-----+-------------+--+----+----------+------+-- ... ----+
+    // File  | r0  |        r1   |P | r2 |    r3    |  r4  |           |
+    //       +-----+-------------+--+----+----------+------+-- ... ----+
+    //       <--- block_size ------>|<-- block_size ------>|
+
+    // rn = variable size records
+    // P = Padding
     allocator: *const Allocator,
     fd: fs.File,
-    max_block_size: usize,
+    block_size: usize,
 
     pub fn init(allocator: *const Allocator, options: WalOptions) !WriteAheadLog {
-        const fd = try options.file_dir.createFile(options.file_name, .{ .truncate = false });
+        const fd = try options.file_dir.createFile(
+            options.file_name,
+            .{ .read = true, .truncate = false },
+        );
+
+        std.debug.assert(options.block_size > Record.header_size());
 
         return WriteAheadLog{
             .allocator = allocator,
             .fd = fd,
-            .max_block_size = options.max_block_size,
+            .block_size = options.block_size,
         };
     }
 
@@ -59,24 +121,88 @@ pub const WriteAheadLog = struct {
         self.fd.close();
     }
 
-    pub fn write(self: *WriteAheadLog, payload: []const u8) !void {
-        const encoded_bytes = self.encode(payload);
-        defer self.allocator.free(encoded_bytes);
-        try self.fd.seekFromEnd(0);
-        try self.fd.writeAll(encoded_bytes);
+    // Reads a block from the file, at current position and decodes it into a list of records
+    pub fn readBlock(self: *WriteAheadLog) ![]Record {
+        const buffer = self.allocator.alloc(u8, self.block_size) catch unreachable;
+        defer self.allocator.free(buffer);
+        _ = try self.fd.readAll(buffer);
+
+        var decode_offset: usize = 0;
+        var records = std.ArrayList(Record).init(self.allocator.*);
+        errdefer records.deinit();
+        while (decode_offset < self.block_size) {
+            const record = Record.decode(buffer[decode_offset..]) catch {
+                log.info("Failed to decode record at offset, hit block padding {d}", .{decode_offset});
+                break;
+            };
+            records.append(record) catch unreachable;
+            decode_offset += record.size();
+        }
+
+        return records.toOwnedSlice();
     }
 
-    fn encode(self: *WriteAheadLog, payload: []const u8) []const u8 {
-        const length = payload.len;
-        const header_length = @sizeOf(u32) + @sizeOf(u16) + @sizeOf(u8);
+    pub fn write(self: *WriteAheadLog, payload: []const u8) !void {
+        const records = self.encode(payload);
+        defer self.allocator.free(records);
 
-        if (length + header_length > self.max_block_size) {
-            var offset: usize = 0;
-            const num_records = length / (self.max_block_size - header_length) + 1;
-            const record_length = self.max_block_size - header_length;
+        for (records) |record| {
+            const curr_end_pos: usize = self.fd.getEndPos() catch unreachable;
+            self.fd.seekFromEnd(0) catch unreachable;
+            var last_block_space_left: usize = 0;
+            if (curr_end_pos < self.block_size) {
+                last_block_space_left = 0; // First block, no last block exists
+            } else {
+                last_block_space_left = self.lastBlockSpaceLeft();
+            }
 
-            const buffer = self.allocator.alloc(u8, num_records * self.max_block_size) catch unreachable;
+            const buffer = self.allocator.alloc(u8, record.size()) catch unreachable;
+            defer self.allocator.free(buffer);
             @memset(buffer, 0);
+            try record.encode(buffer);
+
+            const fd_writer = self.fd.writer();
+
+            if (record.size() <= last_block_space_left) {
+                // We can fit the record in the last block
+                self.fd.seekBy(-@as(i64, @intCast(last_block_space_left))) catch unreachable;
+                try fd_writer.writeAll(buffer);
+                try fd_writer.writeByteNTimes(0, last_block_space_left - record.size());
+            } else {
+                // We need to write to a new block
+                try fd_writer.writeAll(buffer);
+                try fd_writer.writeByteNTimes(0, self.block_size - record.size());
+            }
+        }
+    }
+
+    fn lastBlockSpaceLeft(self: *WriteAheadLog) usize {
+        const seek_back: i64 = -@as(i64, @intCast(self.block_size));
+        const curr_pos = self.fd.getPos() catch unreachable;
+
+        self.fd.seekFromEnd(seek_back) catch unreachable;
+        const last_block_records = self.readBlock() catch |err| {
+            log.err("Error reading last block: {s}\n{?}", .{ @errorName(err), @errorReturnTrace() });
+            return 0;
+        };
+        defer self.allocator.free(last_block_records);
+        var last_block_offset: usize = 0;
+        for (last_block_records) |record| {
+            last_block_offset += record.size();
+        }
+        self.fd.seekTo(curr_pos) catch unreachable;
+        return self.block_size - last_block_offset;
+    }
+
+    fn encode(self: *WriteAheadLog, payload: []const u8) []Record {
+        const length = payload.len;
+        const header_length = Record.header_size();
+
+        if (length + header_length > self.block_size) {
+            var offset: usize = 0;
+            const num_records = length / (self.block_size - header_length) + 1;
+            const record_length = self.block_size - header_length;
+            const records = self.allocator.alloc(Record, num_records) catch unreachable;
 
             while (offset < payload.len) {
                 var curr_payload: []const u8 = undefined;
@@ -86,7 +212,6 @@ pub const WriteAheadLog = struct {
                     curr_payload = payload[offset..(offset + record_length)];
                 }
 
-                const curr_checksum = std.hash.Crc32.hash(curr_payload);
                 var curr_type: RecordType = undefined;
                 if (offset == 0) {
                     curr_type = .FIRST;
@@ -96,31 +221,65 @@ pub const WriteAheadLog = struct {
                     curr_type = .MIDDLE;
                 }
 
-                const curr_record = Record{
-                    .checksum = curr_checksum,
-                    .length = curr_payload.len,
-                    .type = curr_type,
-                    .data = curr_payload,
-                };
-
-                curr_record.encode(buffer[offset..]) catch unreachable;
+                const curr_record = Record.init(curr_payload, curr_type);
+                records[offset / record_length] = curr_record;
                 offset += record_length;
             }
 
-            return buffer;
+            return records;
         } else {
-            const checksum = std.hash.Crc32.hash(payload);
-            const record = Record{
-                .checksum = checksum,
-                .length = payload.len,
-                .type = .FULL,
-                .data = payload,
-            };
-
-            const buffer = self.allocator.alloc(u8, self.max_block_size) catch unreachable;
-            @memset(buffer, 0);
-            record.encode(buffer) catch unreachable;
-            return buffer;
+            const record = Record.init(payload, .FULL);
+            const records = self.allocator.alloc(Record, 1) catch unreachable;
+            records[0] = record;
+            return records;
         }
     }
 };
+
+test "Record encode/decode" {
+    const allocator = std.testing.allocator;
+    const record = Record{
+        .checksum = 0x12345678,
+        .length = 5,
+        .record_type = .FULL,
+        .data = "hello",
+    };
+
+    const buffer = try allocator.alloc(u8, record.size());
+    defer allocator.free(buffer);
+    try record.encode(buffer);
+
+    const decoded_record = try Record.decode(buffer);
+    try testing.expect(std.mem.eql(u8, record.data, decoded_record.data));
+    try testing.expectEqual(record.checksum, decoded_record.checksum);
+    try testing.expectEqual(record.length, decoded_record.length);
+    try testing.expectEqual(record.record_type, decoded_record.record_type);
+}
+
+test "WriteAheadLog write" {
+    fs.cwd().deleteFile("test.wal") catch |err| switch (err) {
+        error.FileNotFound => {},
+        else => return err,
+    };
+
+    const block_size = 32 * @sizeOf(u8);
+
+    const allocator = std.testing.allocator;
+    var wal = try WriteAheadLog.init(&allocator, .{
+        .file_dir = fs.cwd(),
+        .file_name = "test.wal",
+        .block_size = block_size,
+    });
+    defer wal.deinit();
+
+    try wal.write("hello, world-1");
+    try wal.write("hello, world-2");
+    try wal.write("hello, world-3");
+    try wal.write("hel0");
+    try wal.write("hello, world");
+    try wal.write("hello, world-6");
+    try wal.write("hel1");
+
+    const stat = try wal.fd.stat();
+    try testing.expectEqual(stat.size, block_size * 5);
+}
